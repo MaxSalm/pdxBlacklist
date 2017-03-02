4171e6570ad6e7f8ab465392ef69378295cfcba3
Max Salm <maxsalm3@gmail.com>
2017-03-02 19:07:38 +0000

    Project initialisation

---
+++ scripts/0_setup_bcbio.sh
@@ -0,0 +1,15 @@
+## This simple script runs the automated bcbio installation script
+# http://bcbio-nextgen.readthedocs.io/en/latest/contents/installation.html
+
+## Variables
+# Please ammend these as required
+ROOT="/usr/local/share"
+GENOME="hg19"
+ALIGNER="bwa"
+
+## Run 
+wget https://raw.github.com/chapmanb/bcbio-nextgen/master/scripts/bcbio_nextgen_install.py
+python bcbio_nextgen_install.py $ROOT/bcbio --genomes $GENOME --aligners $ALIGNER --distribution ubuntu --tooldir=$ROOT/bcbio
+bcbio_nextgen.py upgrade -u stable 
+
+
