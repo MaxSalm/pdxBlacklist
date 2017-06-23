5ab65a5502cd1fdccd3f3f8daff4032ca262e6e8
Max Salm <maxsalm3@gmail.com>
2017-06-23 18:11:35 +0100

    Moved to scripts lib

---
+++ scripts/retrieve_public_data.py
@@ -0,0 +1,131 @@
+__author__ = 'Max Salm'
+__date__ = '23/06/2017'
+__licence__ = "GPL"
+
+### Generate an in silico PDx WGS run using SRA data
+
+## Run example
+# python retrieve_public_data.py --proportion
+
+### Prerequisites
+## Install cutadapt
+# conda install -c bioconda cutadapt
+## Install sratoolkit
+# conda install sra-tools=2.8.1
+## Install seqtk
+# git clone https://github.com/lh3/seqtk.git;
+# cd seqtk; make
+
+
+########################
+### System libraries ###
+########################
+
+import os
+import sys
+import argparse
+
+USE_CMDLINE = True ## Simple toggle to simplify debugging
+
+if USE_CMDLINE:
+    parser = argparse.ArgumentParser(description='Generate an in silico PDx WGS run using SRA data',
+                                     prog='Generate an in silico PDx WGS run using SRA data',
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-p', help = 'Percentage of human reads in final output [0-100]', type=int)
+
+    ## Gather arguments
+    args = parser.parse_args()
+    PROPORTION = float(args.p) / 100.0
+else:
+    PROPORTION = 0.5
+
+print "\n\tProportion of human reads:" + str(PROPORTION)
+
+########################
+### Global Variables ###
+########################
+FINALYIELD = 10000 # Number of reads in final file
+
+#################
+### Functions ###
+#################
+
+def getSRA(srr, output, trim=True):
+    """
+    Download WGS data from SRA. Human, NA12878, https://trace.ncbi.nlm.nih.gov/Traces/sra/?study=ERP001960
+    Download WGS data from SRA. Mouse, NOD/ShiLtJ, https://www.ncbi.nlm.nih.gov/sra/?term=ERP000927
+
+    :srr: SRA identifier for fastq-dump
+    :output: FASTQ file prefix
+    :trim: Trim the fastq data?
+
+    :return: None
+    """
+
+    f1 = output + "_1.fastq.gz"
+    f2 = output + "_2.fastq.gz"
+    srr_1 = srr + "_1.fastq.gz"
+    srr_2 = srr + "_2.fastq.gz"
+
+    ## Download data
+    if not os.path.exists(f1) and not os.path.exists(f2):
+        EXE = "fastq-dump"
+        print "Processing:" + srr
+        exe_h = [EXE, "--split-files", "--clip", "--gzip", srr]
+        print ' '.join(exe_h)
+        os.system(' '.join(exe_h))
+
+        ## Trim the additional base
+        if trim:
+            EXE = "cutadapt"
+            exe_c = [EXE, "-u", "-1", "-o", f1, "-p", f2, srr_1, srr_2]
+            os.system(' '.join(exe_c))
+        else:
+            os.rename(srr_1, f1)
+            os.rename(srr_2, f2)
+
+    return None
+
+
+
+def createPDx():
+    """
+     Subsample and Merge the outputs, at pre-specified proportions.
+
+     :return: None
+     """
+
+    ROOT = "pdx_synthetic_" + str(args.p)
+
+    ## Subsample 10000 read pairs from two large paired FASTQ files (remember to use the same random seed to keep pairing):
+    EXE = "~/seqtk/seqtk sample"
+    exe_h1 = [EXE, "-s100", "NA12878_1.fastq.gz", str(int(FINALYIELD * 2 * PROPORTION)), ">", ROOT+"hsub1.fastq"]
+    exe_h2 = [EXE, "-s100", "NA12878_2.fastq.gz", str(int(FINALYIELD * 2 * PROPORTION)), ">", ROOT+"hsub2.fastq"]
+    os.system(' '.join(exe_h1))
+    os.system(' '.join(exe_h2))
+
+    exe_m1 = [EXE, "-s100", "NODShiLtJ_1.fastq.gz", str(int(FINALYIELD * 2 * (1 - PROPORTION) )), ">", ROOT+"msub1.fastq"]
+    exe_m2 = [EXE, "-s100", "NODShiLtJ_2.fastq.gz", str(int(FINALYIELD * 2 * (1 - PROPORTION) )), ">", ROOT+"msub2.fastq"]
+    os.system(' '.join(exe_m1))
+    os.system(' '.join(exe_m2))
+
+    ## Concatenate
+    EXE = "cat"
+    exe_c = [EXE, ROOT+"hsub1.fastq", ROOT+"msub1.fastq", ">", ROOT + "_1.fastq"]
+    os.system(' '.join(exe_c))
+    exe_c = [EXE, ROOT+"hsub2.fastq", ROOT+"msub2.fastq", ">", ROOT + "_2.fastq"]
+    os.system(' '.join(exe_c))
+
+    ## CLean-up
+    # os.remove(ROOT+"hsub1.fastq")
+    # os.remove(ROOT+"hsub2.fastq")
+    # os.remove(ROOT+"msub1.fastq")
+    # os.remove(ROOT+"msub2.fastq")
+
+if __name__ == '__main__':
+    getSRA(srr="ERR194147", output="NA12878", trim=True)
+    getSRA(srr="ERR118255", output="NODShiLtJ", trim=False)
+    createPDx()
+
+
+
